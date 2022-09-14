300faa158257d2b2f6097ee8900e922c182433f9
Mariano Kuran <marianokuran6@gmail.com>
2022-09-14 20:29:16 -0300

    se creó el componente landing page

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,23 @@
-import './App.css';
-import {useDispatch} from "react-redux";
-import {getState} from "./redux/actions";
+import "./App.css";
+import { useDispatch } from "react-redux";
+import { getState } from "./redux/actions";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+import Landing from "./components/Landing";
 
 function App() {
-
-    const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
   return (
-    <div className="App">
-      <h1>MUNDIAL</h1>
-        <button onClick={() => dispatch(getState())} >test</button>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Landing} />
+      </Switch>
+      <div className="App">
+        <h1>MUNDIAL</h1>
+        <button onClick={() => dispatch(getState())}>test</button>
+      </div>
+    </BrowserRouter>
   );
 }
 
