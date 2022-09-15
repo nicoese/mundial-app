da211c1c9c3a71e64f32495650c5904d537f88d5
Gaston Fernandez <gastyfp@gmail.com>
2022-09-15 18:16:47 -0300

    splitting routes into dif files, modifying models, still needs improvement

--- api/src/routes/Products.js
+++ api/src/routes/Products.js
@@ -3,7 +3,9 @@ const router = Router();
 const Accesories = require('../models/accesories')
 const Jerseys = require('../models/jerseys');
 const Tickets = require('../models/tickets')
-
+const Purchase = require('../models/purchases')
+const User = require('../models/users');
+const db = require('../../db.json')
 
 
 router.get('/', async (req,res,next)=>{
@@ -20,5 +22,32 @@ router.get('/', async (req,res,next)=>{
         next(err)
     }
 })
+
+
+//esta ruta existe solo con el proposito de insertar todos los datos en la BD, no utilizar
+router.post('/insert_products',async (req,res,next)=>{
+    try{
+        insertProducts();
+    }catch(err){
+        next(err)
+    }
+})
+
+
+//funcion que inserta todos los productos del db.json a mongo en cantidad.
+const insertProducts = async ()=>{
+    try{
+        let acc = db.products.accessories
+        let jer = db.products.jerseys
+        let tick = db.products.tickets
+        const resacc = await Accesories.insertMany(acc)
+        const resjer = await Jerseys.insertMany(jer)
+        const restick = await Tickets.insertMany(tick)
+    }
+    catch(err){
+        console.log(err)
+    }
+}
+
 
 module.exports = router;
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -3,6 +3,8 @@ const { Router } = require('express');
 // Ejemplo: const authRouter = require('./auth.js');
 
 const productsRoute = require('./Products')
+const usersRoute = require('./Users')
+const purchasesRoute = require('./Purchases')
 
 
 const router = Router();
@@ -10,6 +12,9 @@ const router = Router();
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 router.use('/products', productsRoute);
+router.use('/users', usersRoute);
+router.use('./purchases',purchasesRoute)
+
 
 
 module.exports = router;
