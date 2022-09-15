0da1ab974fa3db0488a1cb9519689c29e835fdda
Nicolas ese <nicolagsalinas2@gmail.com>
2022-09-14 21:19:26 -0300

    tailwind + product_container

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,27 @@
 import './App.css';
+import './assets/main.css'
+import './assets/tailwind.css'
+import {Route} from "react-router-dom";
+import {ProductsContainer} from "./react/components/ProductsContainer";
 import {useDispatch} from "react-redux";
-import {getState} from "./redux/actions";
+import {useEffect} from "react";
+import {getAllProducts, setCurrentProducts, shuffleProducts} from "./redux/actions";
 
 function App() {
 
     const dispatch = useDispatch()
 
-  return (
-    <div className="App">
-      <h1>MUNDIAL</h1>
-        <button onClick={() => dispatch(getState())} >test</button>
-    </div>
-  );
+    useEffect(async () => {
+        await dispatch(getAllProducts())
+        dispatch(shuffleProducts())
+        dispatch(setCurrentProducts())
+    }, [])
+
+    return (
+        <div className="App">
+                <Route exact path={'/'} component={ProductsContainer}/>
+        </div>
+    );
 }
 
 export default App;
--- client/src/redux/reducer/index.js
+++ client/src/redux/reducer/index.js
@@ -1,17 +1,35 @@
-import {} from "../actions"
+import {GET_ALL_PRODUCTS, SET_CURRENT_PRODUCTS, SHUFFLE_PRODUCTS} from "../actions"
 
 const initialState = {
-    test_state: ''
+    products: [],
+    productsPerPage: 20,
+    currentPage: 0,
+    currentProducts: [],
+    ProductDetail: {},
+
 }
 
 export const rootReducer = (state = initialState, action) => {
     switch (action.type) {
 
-        case 'test':
+        case GET_ALL_PRODUCTS:
             return {
                 ...state,
-                test_state: action.payload
+                products: [...action.payload.jerseys, ...action.payload.accessories, ...action.payload.tickets],
             }
+
+        case SET_CURRENT_PRODUCTS:
+            return {
+                ...state,
+                currentProducts: state.products,
+                currentPage: action.payload
+            }
+        case SHUFFLE_PRODUCTS:
+            return {
+                ...state,
+                products: state.products.sort((a, b) => 0.5 - Math.random())
+            }
+
 
         default:
             return state
