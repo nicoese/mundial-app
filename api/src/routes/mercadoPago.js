af205c5fcdcca57bd842bc3383bb9192ccdee0e0
Nicolas Larrosa <enzolarrosa@gmail.com>
2022-09-23 17:00:18 -0300

    Terminada pasarela papaaaaa

--- api/src/routes/mercadoPago.js
+++ api/src/routes/mercadoPago.js
@@ -1,14 +1,34 @@
 const {Router} = require('express');
 const router = Router();
+const PaymentService = require('../service/PaymentService.js')
 
-router.get("/payment", async (req, res) => {
+router.get("/", async (req, res) => {
     try {
-      const { tournamentid, userid, useremail } = req.query;
-      const response = await PaymentService(price,);
-      res.send(response.data.init_point);
+      const {userEmail } = req.query.userEmail;
+      const items  = req.body;
+      const response = await PaymentService(userEmail,items);
+      console.log(response.data)
+      return res.send(response.data.init_point);
     } catch (error) {
       res.send(error);
     }
   });
   
-  export default router;
+
+//   {
+//     "id": 1203699430,
+//     "nickname": "TESTIXZS4XGN",
+//     "password": "USndyFvVzg",
+//     "site_status": "active",
+//     "email": "test_user_29946263@testuser.com"
+// }
+
+// {
+//   "id": 1203698211,
+//   "nickname": "TETE1258060",
+//   "password": "og1AXV51FL",
+//   "site_status": "active",
+//   "email": "test_user_51813153@testuser.com"
+// }
+
+module.exports= router;
--- api/src/service/PaymentService.js
+++ api/src/service/PaymentService.js
@@ -1,27 +1,46 @@
 const axios = require("axios");
 
-class PaymentService {
-  async createPayment(price) {
+async function PaymentService(userEmail,items) {
     const url = "https://api.mercadopago.com/checkout/preferences";
 
     const body = {
-      payer_email: "test_user_46945293@testuser.com",
-      items: [
-        {
-          title: "Carrito de compra de MundiApp",
-          description: "Una seleccion de producto/s que usted elijío",
-          picture_url: "http://www.myapp.com/myimage.jpg",
-          category_id: "category123",
-          quantity: 1,
-          unit_price: price
-        }
-      ],
+      payer_email: userEmail,
+      items: items.map(e => {return {
+        title: "detalle",
+        description: "Productos de MundiApp",
+        picture_url: e.img,
+        category_id: "category123",
+        quantity: e.cantidad,
+        unit_price: e.price,
+      }})
+      ,
       back_urls: {
-        failure: "/failure",
-        pending: "/pending",
-        success: `${process.env.CLIENT_URL}`
+        failure: `${process.env.CLIENT_URL}/purchases/failed`,
+        pending: `${process.env.CLIENT_URL}/cart`,
+        success: `${process.env.CLIENT_URL}/purchases/success`
       }
+      
     };
+
+    // const body = {
+    //   payer_email: userEmail,
+    //   items: items.map(e => {
+    //     return { 
+    //     title: e.name,
+    //     description: "Productos de MundiApp",
+    //     picture_url: "http://www.myapp.com/myimage.jpg",
+    //     category_id: "category123",
+    //     quantity: 1,
+    //     unit_price: e.price,
+    //     }})
+    //   ,
+    //   back_urls: {
+    //     failure: "/failure",
+    //     pending: "/pending",
+    //     success: `${process.env.CLIENT_URL}`
+    //   }
+      
+    // };
 
     const payment = await axios.post(url, body, {
       headers: {
@@ -30,11 +49,27 @@ class PaymentService {
       }
     });
 
-    return payment.data;
-  }
-}
+    console.log(payment.data)
 
-// CLIENT_URL=www.mundi-app.tk/products
+    return payment;
+  }
+
+// CLIENT_URL=www.mundi-app.tk
 // ACCESS_TOKEN=APP_USR-6804717935567762-092201-d70745a657fed9dae04c97084617a743-129914721
+//   {
+//     "id": 1203699430,
+//     "nickname": "TESTIXZS4XGN",
+//     "password": "USndyFvVzg",
+//     "site_status": "active",
+//     "email": "test_user_29946263@testuser.com"
+// }
+
+// {
+//   "id": 1203698211,
+//   "nickname": "TETE1258060",
+//   "password": "og1AXV51FL",
+//   "site_status": "active",
+//   "email": "test_user_51813153@testuser.com"
+// }
 
 module.exports = PaymentService;
