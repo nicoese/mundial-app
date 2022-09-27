e51d8f61fa1beb36e378e0b03e77aba3b7a94dc3
Mariano Kuran <marianokuran6@gmail.com>
2022-09-27 19:52:06 -0300

    añado la rutae get para el carrito

--- api/src/routes/Carts.js
+++ api/src/routes/Carts.js
@@ -29,20 +29,21 @@ router.post('/add_to_cart', async (req,res,next)=>{
         // if not added we insert in the array of prods the new product
         if(!arr.find(e=>e.name === product.name)){
             arr.push(product)
-        }else{
-        //if product is already there check the quantity
-            let newArr = arr.map(e=>{
-                if(e.name === product.name){
-                    if(e.cantidad !== product.cantidad){
-                        e.cantidad = product.cantidad
-                    }
-                }
-            })
         }
+            //if product is already there check the quantity
+        let newArr = arr.map(e=>{
+            if(e.name === product.name){
+                if(e.cantidad !== product.cantidad){
+                    return {...e,cantidad: product.cantidad}
+                }
+            }
+            return e
+        })
+        
 
         //set new array in cart
 
-        let setCart = user[0].set({products: arr})
+        let setCart = user[0].set({products: newArr})
         
         let result = await setCart.save()
 
@@ -50,10 +51,7 @@ router.post('/add_to_cart', async (req,res,next)=>{
     }catch(err){
         next(err)
     }
-
-
 })
-
 
 router.put('/remove_from_cart', async (req,res,next)=>{
     try{
@@ -62,8 +60,9 @@ router.put('/remove_from_cart', async (req,res,next)=>{
         let user = await Cart.find({email: userEmail})
 
         if (user.length !== 0){
-
-            let filtered = user[0].products.filter(e=>e._id !== productId)
+            
+            /* console.log("back", user[0].products[0].id, productId ); */
+            let filtered = user[0].products.filter(e=>e.id !== productId)
 
             let setCart = user[0].set({products: filtered})
 
@@ -74,7 +73,22 @@ router.put('/remove_from_cart', async (req,res,next)=>{
     }catch(err){
         next(err)
     }
+})
 
+router.get('/', async (req,res,next)=>{
+    try{
+        const {email}= req.query
+
+        console.log(email);
+        
+        let user = await Cart.find({email})
+
+        console.log(user);
+        return res.status(200).json(user[0].products)
+        
+    }catch(err){
+        next(err)
+    }
 })
 
 
