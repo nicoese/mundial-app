b10f41e35bb95b2a29759973269a9ffe10d1a4a7
Gaston Fernandez <gastyfp@gmail.com>
2022-10-01 12:00:12 -0300

    erasing user route useless

--- api/src/routes/Users.js
+++ api/src/routes/Users.js
@@ -12,23 +12,6 @@ router.get('/', async (req,res,next)=>{
     }
 })
 
-//Needs to be modified?
-// router.post('/create_user', async (req,res,next)=>{
-//     try{
-//         const {first_name,last_name} = req.body
-//         console.log(first_name);
-//         let user = new User({
-//             first_name,
-//             last_name,
-//             img: 'imagen',
-//         })
-//         const savedUser = await user.save()
-//         res.status(200).json(savedUser)
-//     }catch(err){
-//         next(err);
-//     }
-// })
-
 //this route modifies user, you can modify your name or the img in your profile
 //we can add more params in body for mods
 router.put('/:userId/modify_user', async (req,res,next)=>{
