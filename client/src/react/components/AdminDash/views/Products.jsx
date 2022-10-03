bef2871967c3e04ebac1b5bda83ff45ae0343429
Gaston Fernandez <gastyfp@gmail.com>
2022-10-03 17:32:07 -0300

    minor change

--- client/src/react/components/AdminDash/views/Products.jsx
+++ client/src/react/components/AdminDash/views/Products.jsx
@@ -47,7 +47,7 @@ const Products = () => {
         }
     },
     { field: 'active', headerName: 'Active', width: 80},
-    {field: 'editar',headerName: 'Editar', width: 70, renderCell: (params)=><EditIcon onClick={()=>handleEdit(params.row.email)} className='text-gray-600 bg-transparent rounded-md hover: cursor-pointer' fontSize='large' />},
+    {field: 'editar',headerName: 'Editar', width: 70, renderCell: (params)=><EditIcon onClick={()=>handleEdit(params.row.email)} className='text-gray-500 bg-transparent rounded-md hover: cursor-pointer' fontSize='large' />},
     {field: 'deshabilitar',headerName: 'Deshabilitar/ Habilitar', width: 180,  renderCell: (params)=>params.row.active?<CancelIcon onClick={()=>handleDisable(params.row._id,false)} className='text-red-600 bg-transparent hover: cursor-pointer rounded-md' fontSize='large' />:<CheckCircleIcon onClick={()=>handleDisable(params.row._id, true)} className='text-green-600 bg-transparent hover: cursor-pointer rounded-md' fontSize='large'  />},
 
     ];
